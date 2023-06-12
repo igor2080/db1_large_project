4b7cedc353e048d37d01f7974745e56aa7f795c7
igor <igoral2080@gmail.com>
2023-06-12 17:24:00 +0300

    fix view file to match view create script

--- Number of branches for each organization.sql
+++ Number of branches for each organization.sql
@@ -1,7 +1,6 @@
 CREATE VIEW [Number of branches for each organization]
 as
-select distinct count(*) 'Number of branches', Organization.OrganizationID
-from Organization
-join LocatedIn on LocatedIn.OrganizationID = Organization.OrganizationID
-group by Organization.OrganizationID
+select distinct count(*) 'Number of branches', OrganizationID
+from Place
+group by OrganizationID
 go
